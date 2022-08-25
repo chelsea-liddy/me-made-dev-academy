94c2ee0cf75ccd44aab52de08bfe2bcac0a3e385
Chelsea Liddy <107088670+chelsea-liddy@users.noreply.github.com>
2022-08-25 12:00:57 +1200

    initial commit

---
+++ client/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <h3 className="author">Chelsea Liddy 2022</h3>
+    </div>
+  )
+}
+
+export default Footer
