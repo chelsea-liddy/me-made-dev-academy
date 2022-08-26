02a71eb509d7b82b401953a9d779aeb0c8df5767
Chelsea Liddy <107088670+chelsea-liddy@users.noreply.github.com>
2022-08-26 20:07:54 +1200

    add nav component

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Header from './Header'
 import Footer from './Footer'
+import Nav from './Nav'
 
 const App = () => {
   return (
     <>
       <Header />
-
+      <Nav />
       <div className="main"></div>
       <Footer />
     </>
