01c309da65a0f88e1b7aa83026469d8f7333c816
Chelsea Liddy <107088670+chelsea-liddy@users.noreply.github.com>
2022-08-28 14:40:32 +1200

    basic nav list set up

--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
 
 const Nav = () => {
-  return <div></div>
+  return (
+    <div className="navbar">
+      <nav>
+        <ul>
+          <li className="navbar-item">Projects</li>
+          <li className="navbar-item">Stash</li>
+          <li className="navbar-item">Inspiration</li>
+        </ul>
+      </nav>
+    </div>
+  )
 }
 
 export default Nav
