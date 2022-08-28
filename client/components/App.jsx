708e276d7e7d729f16e14dc10b51d6138c780db8
Chelsea Liddy <107088670+chelsea-liddy@users.noreply.github.com>
2022-08-28 14:55:10 +1200

    build layout of dashboard

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,13 +2,17 @@ import React from 'react'
 import Header from './Header'
 import Footer from './Footer'
 import Nav from './Nav'
+import Dashboard from './Dashboard'
 
 const App = () => {
   return (
     <>
       <Header />
       <Nav />
-      <div className="main"></div>
+      <div className="main">
+        <Dashboard />
+      </div>
+
       <Footer />
     </>
   )
---
+++ client/components/Dashboard.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Dashboard = () => {
+  return (
+    <div className="dashboard">
+      <h1>Dashboard</h1>
+      <div>
+        <h2>Title of last project updated</h2>
+        <img
+          src="https://images4-f.ravelrycache.com/uploads/trulymyrtle/664603751/georgie2_small2.jpg"
+          alt="georgie sweater"
+        ></img>
+        <p>Latest update: Up to round 6 of right sleeve</p>
+      </div>
+    </div>
+  )
+}
+
+export default Dashboard
