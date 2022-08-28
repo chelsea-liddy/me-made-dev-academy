4b0d1a7e846a86e5a5d17495f7ed16eca0b21577
Chelsea Liddy <107088670+chelsea-liddy@users.noreply.github.com>
2022-08-28 15:07:06 +1200

    general layout of stash

---
+++ client/components/Stash.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Stash = () => {
+  return (
+    <div className="stash">
+      <h1>Stash</h1>
+      <div>
+        <h2>Stash item 1</h2>
+        <img
+          src="https://cdn.shopify.com/s/files/1/0275/2849/5179/products/Heavy-Gingham-Olive-web.jpg?v=1646010914"
+          alt="green and white checked fabric"
+        ></img>
+        <h3>Description</h3>
+        <p>100% linen</p>
+        <h3>Quantity</h3>
+        <p>3m</p>
+      </div>
+    </div>
+  )
+}
+
+export default Stash
