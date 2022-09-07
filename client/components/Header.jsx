c2a7a1c6423a6cadb6b58889c31f34e460e8b0e0
Chelsea Liddy <107088670+chelsea-liddy@users.noreply.github.com>
2022-09-07 20:31:53 +1200

    center up header

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -5,14 +5,17 @@ import { Link } from 'react-router-dom'
 const Header = () => {
   return (
     <div className="header">
-      <Nav />
-      <Link to="/">
-        <img
-          className="logo"
-          src={'./images/memadelogo.JPG'}
-          alt="me made logo"
-        ></img>
-      </Link>
+      <div className="header-wrapper">
+        <Nav />
+        <Link to="/">
+          <img
+            className="logo"
+            src={'./images/memadelogo.JPG'}
+            alt="me made"
+          ></img>
+        </Link>
+        <div className="emptyDiv"></div>
+      </div>
     </div>
   )
 }
