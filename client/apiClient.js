d9adeb0baa09ee2c678a7ff15b687f9831170b77
Chelsea Liddy <107088670+chelsea-liddy@users.noreply.github.com>
2022-09-08 12:25:13 +1200

    call getProject api

--- client/apiClient.js
+++ client/apiClient.js
@@ -3,3 +3,7 @@ import request from 'superagent'
 export function getProjects() {
   return request.get('/v1/projects').then((res) => res.body)
 }
+
+export function getProject(id) {
+  return request.get(`/v1/projects/${id}`).then((res) => res.body)
+}
